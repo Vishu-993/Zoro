7f9280002d2645231798b1f0dcaafc753cdb00ba
Vishu-993 <116909259+Vishu-993@users.noreply.github.com>
2023-06-22 14:28:14 +0530

    Update progress.py

--- helper/progress.py
+++ helper/progress.py
@@ -2,18 +2,31 @@ import math
 import time
 
 
+class ProgressBar:
+    def __init__(self, total, width=40):
+        self.total = total
+        self.width = width
+
+    def update(self, progress):
+        filled_length = math.ceil(self.width * progress / self.total)
+        empty_length = self.width - filled_length
+        bar = '█' * filled_length + '░' * empty_length
+        percentage = round(progress / self.total * 100, 2)
+        progress_bar = f'[{bar}] {percentage}%'
+        return progress_bar
+
+
 async def progress_for_pyrogram(
     current,
     total,
     ud_type,
     message,
-    start
+    start,
+    cancel_button=False
 ):
-
     now = time.time()
     diff = now - start
     if round(diff % 10.00) == 0 or current == total:
-        # if round(current / total * 100, 0) % 5 == 0:
         percentage = current * 100 / total
         speed = current / diff
         elapsed_time = round(diff) * 1000
@@ -23,33 +36,27 @@ async def progress_for_pyrogram(
         elapsed_time = TimeFormatter(milliseconds=elapsed_time)
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
-        progress = get_progress_bar(percentage)
+        progress = ProgressBar(total)
+        progress_bar = progress.update(current)
 
         tmp = "╭━━━━❰ PROGRESS BAR ❱━➣\n" + \
               f"┣⪼ 🗂️ : {humanbytes(current)} | {humanbytes(total)}\n" + \
               f"┣⪼ ⏳️ : {round(percentage, 2)}%\n" + \
               f"┣⪼ 🚀 : {humanbytes(speed)}/s\n" + \
-              f"┣⪼ ⏱️ : {estimated_total_time}\n" + \
-              f"╰━━━━━━━━━━━━━━━➣ {progress}"
+              f"┣⪼ ⏱️ : {elapsed_time} / {estimated_total_time}\n" + \
+              f"┣⪼ {progress_bar}\n"
+
+        if cancel_button:
+            tmp += "┣⪼ ❌ Cancel"
+        else:
+            tmp += "┣⪼ ⏹️ Cancel"
 
         try:
             await message.edit(
-                text="{}\n {}".format(
-                    ud_type,
-                    tmp
-                )
+                text="{}\n{}".format(ud_type, tmp)
             )
         except:
             pass
-
-
-def get_progress_bar(percentage: float) -> str:
-    filled = math.floor(percentage / 5)
-    empty = 20 - filled
-    return "[{0}{1}]".format(
-            ''.join(["●" for i in range(filled)]),
-            ''.join(["○" for i in range(empty)])
-        )
 
 
 def humanbytes(size):
@@ -72,8 +79,7 @@ def TimeFormatter(milliseconds: int) -> str:
     hours, minutes = divmod(minutes, 60)
     days, hours = divmod(hours, 24)
     tmp = ((str(days) + "d, ") if days else "") + \
-        ((str(hours) + "h, ") if hours else "") + \
-        ((str(minutes) + "m, ") if minutes else "") + \
-        ((str(seconds) + "s, ") if seconds else "") + \
-        ((str(milliseconds) + "ms, ") if milliseconds else "")
-    return tmp[:-2]
+          ((str(hours) + "h, ") if hours else "") + \
+          ((str(minutes) + "m, ") if minutes else "") + \
+          ((str(seconds) + "s, ") if seconds else "") + \
+          ((str(milliseconds) + "ms, ") if
