c502005443dbc2b64bd81f35dcb7bfa87cd773ca
Vishu-993 <116909259+Vishu-993@users.noreply.github.com>
2023-06-24 12:32:17 +0530

    Update caption.py

--- plugins/caption.py
+++ plugins/caption.py
@@ -5,11 +5,7 @@ from helper.database import *
 @Client.on_message(filters.private & filters.command('set_caption'))
 async def add_caption(client, message):
     if len(message.command) == 1:
-       return await message.reply_text("**♻️ Custom Caption ♻️/n/nBy default all messages will be forwarded with its default captions
-Instead of this, you can set a custom caption/n/nFilling options:
-{name} : file name
-{size} : file size
-{caption} : default caption of messages`**")
+       return await message.reply_text("**Give me a caption to set.\n\nExample:- `/set_caption File Name`**")
     caption = message.text.split(" ", 1)[1]
     addcaption(int(message.chat.id), caption)
     await message.reply_text("**Your Caption successfully added ✅**")
