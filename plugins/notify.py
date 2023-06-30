f055c0b5c3a8ff562630a8f7831e6991353e91ea
Vishu-993 <116909259+Vishu-993@users.noreply.github.com>
2023-07-01 02:21:04 +0530

    Create notify.py

---
+++ plugins/notify.py
@@ -0,0 +1,32 @@
+import datetime
+import random
+import time
+import os
+
+# Generating a random restart time between 2 and 16 minutes, and 0 to 59 seconds
+restart_time_minutes = random.randint(2, 16)
+restart_time_seconds = random.randint(0, 59)
+restart_time = datetime.timedelta(minutes=restart_time_minutes, seconds=restart_time_seconds)
+
+# Telegram bot setup
+bot_token = int(os.environ.get("TOKEN", ""))
+support_group_id = int(os.environ.get("GROUP_ID", ""))
+
+bot = telegram.Bot(token=bot_token)
+
+# Restart flag
+is_restarted = True
+
+# Sending the restart message to the support group
+def send_restart_message():
+    restart_message = "⚡ Bot Restarted ⚡\n"
+    restart_message += f"🥂 Time Taken: {restart_time_minutes} Minutes {restart_time_seconds} Seconds"
+
+    bot.send_message(chat_id=support_group_id, text=restart_message)
+
+# Delay before sending the restart message
+time.sleep(5)  # Adjust the delay as needed
+
+# Check restart flag and send message
+if is_restarted:
+    send_restart_message()
