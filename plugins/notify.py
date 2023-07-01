3e128c53c1752adf9cdc3799ed2d7925686c5441
Vishu-993 <116909259+Vishu-993@users.noreply.github.com>
2023-07-01 08:59:20 +0530

    Update notify.py

--- plugins/notify.py
+++ plugins/notify.py
@@ -1,7 +1,8 @@
-import os
+import osimport os
 import datetime
 import random
 import time
+from pyrogram import Client
 
 # Generating a random restart time between 2 and 16 minutes, and 0 to 59 seconds
 restart_time_minutes = random.randint(2, 16)
@@ -15,6 +16,9 @@ support_group_id = int(os.environ.get("GROUP_ID", ""))
 # Restart flag
 is_restarted = True
 
+# Create an instance of the bot
+bot = Client("my_bot", bot_token=bot_token)
+
 # Sending the restart message to the support group
 def send_restart_message():
     restart_message = "⚡ Bot Restarted ⚡\n"
@@ -27,4 +31,5 @@ time.sleep(5)  # Adjust the delay as needed
 
 # Check restart flag and send message
 if is_restarted:
-    send_restart_message()
+    with bot:
+        send_restart_message()
